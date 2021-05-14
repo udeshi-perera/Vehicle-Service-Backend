3bd07b5c13708c378c1f85a9352b9c68b37c2703
udeshiperera <budeshimperera@gmail.com>
2021-05-14 20:49:04 +0530

    add socker cluster service

---
+++ socketcluster-server/ssnotification.js
@@ -0,0 +1,42 @@
+// socketcuster-server
+//this is a simple example socketcluster
+const http = require("http");
+const socketClusterServer = require("socketcluster-server");
+
+let options = {
+  // ...
+};
+
+let httpServer = http.createServer();
+let agServer = socketClusterServer.attach(httpServer, options);
+// --- in server.js ---
+
+// SocketCluster/WebSocket connection handling loop.
+(async () => {
+  for await (let { socket } of agServer.listener("connection")) {
+    (async () => {
+      // Set up a loop to handle remote transmitted events.
+      //give any channel name as u like
+      for await (let data of socket.receiver("channelName")) {
+        //view data from
+        console.log(data);
+
+        //send notifications to client repeatedly
+        try {
+          // Publish data; wait for an acknowledgement from the back end broker (if it exists).
+          setInterval(() => {
+            agServer.exchange.transmitPublish(
+              "channelName",
+              "This is some data sended from server " + Math.random()
+            );
+          }, 700000);
+        } catch (error) {
+          // ... Handle potential error if broker does not acknowledge before timeout.
+        }
+      }
+    })();
+  }
+})();
+
+// port 8000
+httpServer.listen(8000);
